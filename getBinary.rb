433eac4b282c2dbc69c560ff463a2900bc7580c2
standthis <standthis@protonmail.ch>
2019-10-06 16:45:03 +0200

    Initial improvements to follow best practice

--- getBinary.rb
+++ getBinary.rb
@@ -1,29 +1,58 @@
 #!/bin/ruby
 require 'open-uri'
-
+# Target webpage
+# https://cache.ruby-lang.org/pub/ruby/2.6/ruby-2.6.5.tar.gz
 class RubyBinary
-  def download(location, version)
-    major = version.split(".")[0]
-    minor = version.split(".")[1]
-    url = "https://cache.ruby-lang.org/pub/ruby/" + major + "." + minor + "/ruby-" + version + ".tar.gz"
-    f = "/" + location + "/" + 'ruby' + version + ".tar.gz"
-    puts url 
-    puts f
-    File.open(f, "wb") do |file| 
+  attr_reader :version 
+
+  def initialize(version)
+    @version = version
+  end
+
+  def download(location)
+    major, minor = splitVersion(version)
+    url = makeURL(major, minor, version)
+    saveFile(location, version, url)
+  end
+
+  private
+
+  def splitVersion(version)
+    if version.split('.').count != 3
+        puts "#{version} is invalid. Expected $.$.$"
+        exit 1
+      end
+      return version.split('.')[0], version.split('.')[1]
+  end
+
+  def makeURL(major, minor, version)
+    "https://cache.ruby-lang.org/pub/ruby/#{major}.#{minor}/ruby-#{version}.tar.gz"
+  end
+
+  def saveFile(location, version, url)
+    saveTo = "/#{location}/ruby-#{version}.tar.gz"
+    File.open(saveTo, "wb") do |file| 
       file.write open(url).read
     end
+    puts "Target save location: #{saveTo}"
+    puts "Downloading from #{url}..."
   end
 
-  meth = ARGV[0]
-  puts meth
-  version = ARGV[1]
-  location = ARGV[2]
-
-  if meth == "download"
-    download(location, version)
-    puts 'success downloaded'
-  end
 end
 
-#ruby_bin = RubyBinary.new("2.3.0")
-#ruby_bin.download("tmp")
+def argparse
+  if ARGV.length != 3
+    puts "#{program_name} method [FILE ...]"
+    exit 1
+  end
+  method, version, location = ARGV[0], ARGV[1], ARGV[2]
+end
+
+method, version, location = argparse
+
+ruby_bin = RubyBinary.new(version)
+
+if method == "download"
+  ruby_bin.download("tmp")
+  puts 'success downloaded'
+end
